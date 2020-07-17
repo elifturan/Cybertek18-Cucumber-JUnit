e790a2fbb0ac2b5b402f79c62a9ad0ef5f351e90
elifturan <elifergucluuu@gmail.com>
2020-07-17 12:38:21 -0400

    commit

---
+++ src/test/java/com/cybertek/step_definitions/TestClassForGit.java
@@ -0,0 +1,13 @@
+package com.cybertek.step_definitions;
+
+public class TestClassForGit {
+    public static void main(String[] args) {
+
+
+        System.out.println("HEllo GitHub");
+
+
+
+
+    }
+}
